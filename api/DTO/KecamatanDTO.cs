9b53021015ec62ec3b0b01ca8fab77b309b596f4
Abdul Masjit <abdulmasjits@gmail.com>
2022-04-23 09:23:52 +0700

    Endpoint master kecamatan

---
+++ api/DTO/KecamatanDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Ewadul.Api.DTO
+{    
+    public class KecamatanDTO
+    {
+        public string Id { get; set; } = null!;
+        public string? Nama { get; set; } 
+        public string? RegencyId { get; set; } 
+    }
+}
