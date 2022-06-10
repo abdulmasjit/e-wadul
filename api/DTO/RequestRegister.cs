7a79d3cf0251a943fb29b03afb50f501e2a67d28
andre-setiawann9 <andreitats.smt6@gmail.com>
2022-06-10 21:03:13 +0700

    register.API update

---
+++ api/DTO/RequestRegister.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ewadul.Api.DTO;
+
+    public class RequestRegister
+    {
+    //public int Id { get; set; }
+    public string? Nama { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? Nik { get; set; }
+    public string? Telepon { get; set; }
+    public string? Alamat { get; set; }
+    public string? Password { get; set; }
+}
+
